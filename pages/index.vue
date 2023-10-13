941b6e9bfa1c318b2fff2425db74ece6c6966589
Haja Randriakoto <43437694+haja-ran@users.noreply.github.com>
2023-10-13 23:26:18 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex min-h-[100dvh] p-6 i">
+    <h1 class="text-7xl font-bold m-auto">
+      Hello world !
+    </h1>
+  </div>
+</template>
---
+++ pages/trpc.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { $client } = useNuxtApp()
+
+const hello = await $client.hello.useQuery({ text: 'client' })
+const examples = await $client.examples.query()
+</script>
+
+<template>
+  <div>
+    <!-- As `superjson` is already pre-configured, we can use `time` as a `Date` object without further deserialization 🎉 -->
+    tRPC Data: "{{ hello.data.value?.greeting }}" send at "{{
+      hello.data.value?.time.toLocaleDateString("en-EN")
+    }}".
+  </div>
+  <pre>{{ { examples } }}</pre>
+</template>
